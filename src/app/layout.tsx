6d81515c1bc1884d29ad1addb268bc8e56bd4af1
vipshehbaz <learnfromshehbaz08@gmail.com>
2025-07-14 20:14:05 +0500

    feat: ui work complete scripting left

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const openSans = Open_Sans({
+  variable: "--font-open-sans",
   subsets: ["latin"],
 });
 
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${montserrat.variable} ${openSans.variable} antialiased`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,171 @@
 import Image from "next/image";
+import JhonPic from "@/../public/assets/person-images/john.jpg";
+import EmmaPic from "@/../public/assets/person-images/Emma.jpg";
+import SophiaPic from "@/../public/assets/person-images/Sophia.jpg";
+import AhmedPic from "@/../public/assets/person-images/ahmed.jpg";
+import AnilPic from "@/../public/assets/person-images/anil.jpg";
+import ArshadPic from "@/../public/assets/person-images/arshad.jpg";
+import LoenPic from "@/../public/assets/person-images/loen.jpg";
+import SalmanPic from "@/../public/assets/person-images/salman-pic.jpg";
+import SaraPic from "@/../public/assets/person-images/sara-pic.jpg";
+import SarthakPic from "@/../public/assets/person-images/sarthak.jpg";
 
 export default function Home() {
+  const Testimonials = [
+    {
+      name: "Jhon",
+      img: JhonPic,
+      tag: "“Adjective”",
+      des: (
+        <>
+          Lorem ipsum dolor sit amet <b>consectetur</b>. A viverra massa
+          molestie <b>velit et eu.</b>
+        </>
+      ),
+    },
+    {
+      name: "Emma",
+      img: EmmaPic,
+      tag: "“Adjective”",
+      des: (
+        <>
+          Lorem ipsum dolor sit amet <b>consectetur</b>. A viverra massa
+          molestie <b>velit et eu.</b>
+        </>
+      ),
+    },
+    {
+      name: "Sophia",
+      img: SophiaPic,
+      tag: "“Adjective”",
+      des: (
+        <>
+          Lorem ipsum dolor sit amet <b>consectetur</b>. A viverra massa
+          molestie <b>velit et eu.</b>
+        </>
+      ),
+    },
+    {
+      name: "Ahmed",
+      img: AhmedPic,
+      tag: "“Adjective”",
+      des: (
+        <>
+          Lorem ipsum dolor sit amet <b>consectetur</b>. A viverra massa
+          molestie <b>velit et eu.</b>
+        </>
+      ),
+    },
+    {
+      name: "Anil",
+      img: AnilPic,
+      tag: "“Adjective”",
+      des: (
+        <>
+          Lorem ipsum dolor sit amet <b>consectetur</b>. A viverra massa
+          molestie <b>velit et eu.</b>
+        </>
+      ),
+    },
+    {
+      name: "Arshad",
+      img: ArshadPic,
+      tag: "“Adjective”",
+      des: (
+        <>
+          Lorem ipsum dolor sit amet <b>consectetur</b>. A viverra massa
+          molestie <b>velit et eu.</b>
+        </>
+      ),
+    },
+    {
+      name: "Loen",
+      img: LoenPic,
+      tag: "“Adjective”",
+      des: (
+        <>
+          Lorem ipsum dolor sit amet <b>consectetur</b>. A viverra massa
+          molestie <b>velit et eu.</b>
+        </>
+      ),
+    },
+    {
+      name: "Salman",
+      img: SalmanPic,
+      tag: "“Adjective”",
+      des: (
+        <>
+          Lorem ipsum dolor sit amet <b>consectetur</b>. A viverra massa
+          molestie <b>velit et eu.</b>
+        </>
+      ),
+    },
+    {
+      name: "Sara",
+      img: SaraPic,
+      tag: "“Adjective”",
+      des: (
+        <>
+          Lorem ipsum dolor sit amet <b>consectetur</b>. A viverra massa
+          molestie <b>velit et eu.</b>
+        </>
+      ),
+    },
+    {
+      name: "Sarthak",
+      img: SarthakPic,
+      tag: "“Adjective”",
+      des: (
+        <>
+          Lorem ipsum dolor sit amet <b>consectetur</b>. A viverra massa
+          molestie <b>velit et eu.</b>
+        </>
+      ),
+    },
+  ];
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div className="wrapper">
+      <h2 className="section-heading mt-10 opacity-90">Testimonials</h2>
+      <div className="pic-container">
+        {[...Testimonials, ...Testimonials].map((item, index) => (
+          <div key={index} className="per-pic-container">
+            <div className="custom-border"></div>
+            <div className="custom-border-last"></div>
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              className="rounded-full relative z-10"
+              src={item.img}
+              alt={`${item.name} pic`}
+              height={100}
+              width={100}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          </div>
+        ))}
+      </div>
+      <div className="mt-8 w-full flex items-center gap-8 overflow-hidden">
+        {Testimonials.map((item, index) => (
+          <div key={index} className="testimonials-container">
+            <h3 className="section-heading">{item.tag}</h3>
+            <p className="mt-5 font-normal text-xl">
+              {item.des}
+            </p>
+            <div className="flex mt-[30px] items-center gap-3">
+              <Image
+                className="rounded-full"
+                src={item.img}
+                alt={`${item.name} pic`}
+                height={100}
+                width={100}
+              />{" "}
+              <div>
+                <p className="font-semibold text-xl leading-[175%] tracking-[0%]">
+                  {item.name}
+                </p>
+                <p className="font-normal text-base leading-[175%]">Designation</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
